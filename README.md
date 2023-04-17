a69e480c0008f895901b811e7aa4cde63fcc6db5
Ghani <portsign@gmail.com>
2023-04-17 09:32:31 +0700

    first commit HTB SUB FINDER

---
+++ README.md
@@ -0,0 +1,32 @@
+# HTB SUB FINDER
+HTB SUB FINDER is a simple bash script to automate subdomain discovery by brute-forcing subdomains using a wordlist. It adds the discovered subdomains to the /etc/hosts file to allow direct access to the subdomains from the terminal.
+
+## Requirements
+FindSub requires the following tools to be installed on your system:
+
+- bash
+- sed
+- curl
+
+### Usage
+To use htbSubFinder, simply run the htbSubFinder.sh script with the following command-line options:
+```
+./findSub.sh -i <target_ip> -d <domain> -w <wordlist> [-t <timeout>]
+-i, --ip: The IP address of the target server.
+-d, --domain: The domain to brute-force subdomains for.
+-w, --wordlist: The wordlist to use for subdomain brute-forcing.
+-t, --timeout: The connection timeout for curl requests (default: 4 seconds).
+-h, --help: Show the help message and exit.
+```
+For example:
+```
+./findSub.sh -i 10.10.11.196 -d stocker.htb -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt
+```
+
+This will start the subdomain discovery process with the specified options.
+
+### Output
+htbSubFinder will output the discovered subdomains to the terminal, as well as adding them to the /etc/hosts file for easy access. If no subdomains are found, FindSub will exit with an error message.
+
+### License
+This project is licensed under the MIT License - see the LICENSE.md file for details.
---
+++ htbSubFinder.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+target_ip=
+domain=
+wordlist=
+timeout=4
+
+cleanup_hosts_file() {
+    if [ -n "$full_domain" ]; then
+        echo "Removing ${full_domain} from /etc/hosts."
+        sed -i "/${full_domain}/d" /etc/hosts
+    fi
+    exit 1
+}
+
+show_help() {
+    echo "Usage: sudo bash $0 [options] -i <target_ip> -d <domain> -w <wordlist>"
+    echo ""
+    echo "Options:"
+    echo "  -d, --domain DOMAIN"
+    echo "  -i, --ip IP address"
+    echo "  -w, --wordlist WORDLIST  Set Wordlist"
+    echo "  -t, --timeout TIMEOUT    Set connection timeout (default: 4)"
+    echo "  -h, --help               Show this help message and exit"
+    echo ""
+    exit 0
+}
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -t|--timeout)
+            timeout=$2
+            shift
+            ;;
+        -i|--ip)
+            target_ip=$2
+            shift
+            ;;
+        -d|--domain)
+            domain=$2
+            shift
+            ;;
+        -w|--wordlist)
+            wordlist=$2
+            shift
+            ;;
+        -h|--help)
+            show_help
+            ;;
+        *)
+            break
+            ;;
+    esac
+    shift
+done
+
+if [ $# -ne 0 ] || [ -z "$target_ip" ] || [ -z "$domain" ] || [ -z "$wordlist" ]; then
+    show_help
+fi
+
+echo ' _  _ _____ ___   ___ _   _ ___ ___ ___ _  _ ___  ___ ___ '
+echo '| || |_   _| _ ) / __| | | | _ | __|_ _| \| |   \| __| _ \'
+echo '| __ | | | | _ \ \__ | |_| | _ | _| | || .` | |) | _||   /'
+echo '|_||_| |_| |___/ |___/\___/|___|_| |___|_|\_|___/|___|_|_\'
+echo 'CODED BY PORTSIGN'
+echo ''
+read -p "Press enter to start scanning..."
+trap 'cleanup_hosts_file' INT
+
+while read subdomain; do
+    full_domain="${subdomain}.${domain}"
+
+    echo -n "Scanning ${full_domain}             "
+    echo -ne "\r"
+
+    echo "${target_ip}    ${full_domain}" >> /etc/hosts
+    response=$(curl -s -o /dev/null -w "%{http_code}" --connect-timeout ${timeout} -I "http://${full_domain}" 2>/dev/null)
+    if [ "$response" == "200" ] || [ "$response" == "302" ]; then
+        echo -e "\033[32mFound\033[0m ${full_domain} (HTTP/${response})."
+        sed -i "/${full_domain}/d" /etc/hosts
+    elif [ "$response" == "" ]; then
+        sed -i "/${full_domain}/d" /etc/hosts
+        echo "Could not connect to ${full_domain}, removing from /etc/hosts."
+    else
+        sed -i "/${full_domain}/d" /etc/hosts
+    fi
+done < $wordlist
+
+echo "Reached end of wordlist, no subdomains found."
+exit 1
